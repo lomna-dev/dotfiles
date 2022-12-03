25811c18bbd1668ee5078ef8b6c410c9d481d960
LomnaInawan <49801371+LomnaInawan@users.noreply.github.com>
2022-12-03 23:52:35 +0530

    Using package.el to install packages in emacs.

---
+++ emacs/init.el
@@ -0,0 +1,10 @@
+;; Set default encoding for files
+;; UTF-8 as default encoding
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+(set-keyboard-coding-system 'utf-8-unix)
+;; do this especially on Windows, else python output problem
+(set-terminal-coding-system 'utf-8-unix)
+
+;; Load the config file
+(org-babel-load-file "~/.emacs.d/config.org")
--- emacs/swiper/targets/obsolete-config.el
+++
@@ -1,3 +0,0 @@
-(add-to-list 'load-path default-directory)
-(require 'counsel)
-(byte-compile-file (expand-file-name "targets/obsolete-config.el"))
